ff3bd0065d019b8bf4f32ea84cacc61f31fadf9c
Vanina Quezada <vaninaquezada@gmail.com>
2021-03-25 14:48:59 -0300

    enviromet produ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDaDxkZHkwvJy0N7o_DVcmMXIx_6vPSZ7I",
+    authDomain: "tplab4-45942.firebaseapp.com",
+    databaseURL: "https://tplab4-45942.firebaseio.com",
+    projectId: "tplab4-45942",
+    storageBucket: "tplab4-45942.appspot.com",
+    messagingSenderId: "544089026720",
+    appId: "1:544089026720:web:12140e7d4d8c437ea759c5"
+  }
 };

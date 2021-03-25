dfbbc6e7c3f8310645f7ef22c652e387513d5f72
Vanina Quezada <vaninaquezada@gmail.com>
2021-03-25 19:31:40 -0300

    se agregan espacio en img home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 const routes: Routes = [
-
+  {path:'', component:HomeComponent },
+  {path:'login', component:LoginComponent }
 ];
 
 @NgModule({
